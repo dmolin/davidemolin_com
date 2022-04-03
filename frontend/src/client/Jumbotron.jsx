8131f8a805178c63eee079af8e32336cbb11debd
dmolin <davide.molin@gmail.com>
2022-04-03 21:34:40 +0100

    more jumbotron work

--- frontend/src/client/Jumbotron.jsx
+++ frontend/src/client/Jumbotron.jsx
@@ -1,15 +1,13 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
 import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar } from "swiper";
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 // this will be moved to the DB and fetched via Apollo client once the UI is done.
-// const projects = [{"_id":"gB7PybJ7fvbqkFFG5","imageBg":"jumbotron/payfriendz-bg.jpg","imageFg":"jumbotron/payfriendz-fg.png","description":"","fgSize":"auto 50%","bgColor":"#000","priority":999,"createdAt":"2021-05-15T06:16:30.188Z","createdBy":"0"},{"_id":"2ztP2rYHn9enXaBFK","imageBg":"jumbotron/bodog-bg.jpg","imageFg":"jumbotron/bodog-fg.png","description":"","fgSize":"auto 90%","bgColor":"#3b3b3b","priority":991,"createdAt":"2021-05-15T06:16:30.202Z","createdBy":"0"},{"_id":"vEQtmpCnbS5Z9bTdm","imageBg":"jumbotron/x-plane-bg.jpg","imageFg":"jumbotron/x-plane-fg.png","fgSize":"auto 70%","bgColor":"#0f2a45","priority":990,"createdAt":"2021-05-15T06:16:30.204Z","createdBy":"0"},{"_id":"cCgLtaEm39HjYzQgs","imageBg":"jumbotron/udemy-bg.jpg","imageFg":"jumbotron/udemy-fg.png","description":"","fgSize":"auto 70%","bgColor":"#fff","priority":980,"createdAt":"2021-05-15T06:16:30.206Z","createdBy":"0"},{"_id":"o5As8fSrPFjArYg6N","imageBg":"jumbotron/policyexpert-bg.jpg","imageFg":"jumbotron/policyexpert-fg.png","description":"","fgSize":"auto 70%","bgColor":"#f9f9f9","priority":979,"createdAt":"2021-05-15T06:16:30.215Z","createdBy":"0"},{"_id":"9DMcExz92HwePrh99","imageBg":"jumbotron/giadacoppi-bg.jpg","imageFg":"jumbotron/giadacoppi-fg.png","description":"","fgSize":"auto 70%","bgColor":"#e5e5e5","priority":978,"createdAt":"2021-05-15T06:16:30.216Z","createdBy":"0"},{"_id":"fLjdg4f4jBDz6mteQ","imageBg":"jumbotron/zeebox-bg.jpg","imageFg":"jumbotron/zeebox-fg.png","description":"","fgSize":"auto 70%","bgColor":"#6f0000","priority":970,"createdAt":"2021-05-15T06:16:30.217Z","createdBy":"0"},{"_id":"zSM6jo4PSRatz4wXo","imageBg":"jumbotron/mindcandy-bg.jpg","imageFg":"jumbotron/mindcandy-fg.png","description":"","fgSize":"auto 70%","bgColor":"#fff","priority":960,"createdAt":"2021-05-15T06:16:30.223Z","createdBy":"0"}];
-const projects = [
-  { _id: "1", bgColor: "#e5e5e5", bgImage: "economist-bg.jpg", fgImage: "economist-fg.png", priority: 1000 },
-  { _id: "2", bgColor: "#ffffff", bgImage: "hubro-bg.jpg", fgImage: "hubro-fg.png", priority: 1000 },
-  { _id: "3", bgColor: "#000", bgImage: "payfriendz-bg.jpg", fgImage: "payfriendz-fg.png", priority: 1000 },
-  { _id: "5", bgColor: "#0f2a45", bgImage: "x-plane-bg.jpg", fgImage: "x-plane-fg.png",  priority: 1000 },
-];
+import projects from "@/shared/fixtures/projects";
 
 const makeImageUrl = img => `url(/img/jumbotron/${img})`;
 
@@ -31,8 +29,12 @@ const Jumbotron = (props) => {
     <section className={`${className} jumbotron-wrapper`} style={wrapperStyle}>
       <div className="h-full text-center jumbotron">
         <Swiper
+          modules={[Navigation, Pagination, Scrollbar]}
           className="h-full"
           onSlideChange={_handleSlideChange}
+          pagination={true}
+          navigation
+          loop
         >
           {projects.map(p => {
             const fgSize = p.fgSize || "auto 100%";
@@ -57,8 +59,6 @@ const Jumbotron = (props) => {
         .jumbotron {
           max-width: 2200px;
           margin: 0 auto;
-        }
-        .jumbotron-image {
         }
       `}</style>
     </section>
---
+++ frontend/src/shared/fixtures/projects.js
@@ -0,0 +1,12 @@
+export default [
+  { _id: "1", bgColor: "#ffffff", bgImage: "hubro-bg.jpg", fgImage: "hubro-fg.png" },
+  { _id: "2", bgColor: "#e5e5e5", bgImage: "economist-bg.jpg", fgImage: "economist-fg.png" },
+  { _id: "3", bgColor: "#000", bgImage: "payfriendz-bg.jpg", fgImage: "payfriendz-fg.png" },
+  { _id: "5", bgColor: "#0f2a45", bgImage: "x-plane-bg.jpg", fgImage: "x-plane-fg.png" },
+  { _id: "6", bgColor: "#3b3b3b", bgImage: "bodog-bg.jpg", fgImage: "bodog-fg.png", fgSize: "auto 90%" },
+  { _id: "7", bgColor: "#fff", bgImage: "udemy-bg.jpg", fgImage: "udemy-fg.png", fgSize: "auto 70%" },
+  { _id: "8", bgColor: "#f9f9f9", bgImage: "policyexpert-bg.jpg", fgImage: "policyexpert-fg.png", fgSize: "auto 70%" },
+  { _id: "9", bgColor: "#e5e5e5", bgImage: "giadacoppi-bg.jpg", fgImage: "giadacoppi-fg.png", fgSize: "auto 70%" },
+  { _id: "10", bgColor: "#6f0000", bgImage: "zeebox-bg.jpg", fgImage: "zeebox-fg.png", fgSize: "auto 70%" },
+  { _id: "11", bgColor: "#fff", bgImage: "mindcandy-bg.jpg", fgImage: "mindcandy-fg.png", fgSize: "auto 70%" }
+];
