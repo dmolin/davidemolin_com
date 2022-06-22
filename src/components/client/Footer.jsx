75f455a24b63d3feb2d3d5f1e28ace1bf94cf203
dmolin <davide.molin@gmail.com>
2022-06-22 07:16:27 +0100

    keep old techs until the new codebase replaces the old one

--- src/components/client/Footer.jsx
+++ src/components/client/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = props => (
   <section className="bg-neutral-600 text-white py-8 text-sm text-center">
     <div className="page-container flex flex-col gap-6">
-      <p className="">== Powered by NextJs+React+TailwindCSS ==</p>
+      <p className="">== Powered by MeteorJS + React + Redux ==</p>
       <p className="">© Copyright 2022 www.davidemolin.com. All rights reserved.</p>
     </div>
   </section>
