bd6b9e27fb3d257eb508f75beb2c073b7900f2d9
dmolin <davide.molin@gmail.com>
2022-07-01 09:55:29 +0100

    fix Vercel compilation error

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ const Document = () => {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-3VLNVYSDPW"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag() {dataLayer.push(arguments)}
+          function gtag() {window.dataLayer.push(arguments)}
           gtag('js', new Date());
           gtag("config", "G-3VLNVYSDPW");
         </script>
