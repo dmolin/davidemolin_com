d1808f0161ed1aa454745d10ee609a0a8eec5766
dmolin <davide.molin@gmail.com>
2022-07-01 09:47:07 +0100

    add analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,15 @@ const Document = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Chivo&family=Overpass:wght@200&display=swap" rel="stylesheet" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3VLNVYSDPW"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3VLNVYSDPW');
+        </script>
       </Head>
       <body>
         <Main />
