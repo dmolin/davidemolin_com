862908f4679dbc1d26db82b8e69e4bada8bda25e
Davide <davide@davidemolin.com>
2026-05-12 08:05:57 +0100

    fix swiper navigation and pagination modules

--- src/components/client/Swiper.jsx
+++ src/components/client/Swiper.jsx
@@ -1,6 +1,6 @@
 export { Swiper, SwiperSlide } from "swiper/react";
-export { Virtual, Navigation, Pagination, Scrollbar } from "swiper";
+export { Virtual, Navigation, Pagination, Scrollbar } from "swiper/modules";
 import "swiper/css";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
